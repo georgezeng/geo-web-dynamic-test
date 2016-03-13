1679dd2e04631e38f12435f1d6faf78290ef8b93
GeorgeZeng <feleio@qq.com>
2016-03-13 17:18:35 +0800

    refactor contact

--- compileJS.sh
+++ compileJS.sh
@@ -1,3 +1,4 @@
-browserify -t [ babelify --presets [ react ] ] src/main/js/$1/main.js -o src/main/webapp/js/$2-react.js
-browserify -g uglifyify src/main/webapp/js/$2-react.js > src/main/webapp/js/$2.min.js
-rm src/main/webapp/js/$2-react.js
+mkdir src/main/webapp/js/$1
+browserify -t [ babelify --presets [ react ] ] src/$1/js/$2/main.js -o src/main/webapp/js/$1/$2-react.js
+browserify -g uglifyify src/main/webapp/js/$1/$2-react.js > src/main/webapp/js/$1/$2.min.js
+rm src/main/webapp/js/$1/$2-react.js
